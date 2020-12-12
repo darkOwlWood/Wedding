228dec2ed691efa85929e2b9f994293e79271833
Daniel Reyes <danielckings@gmail.com>
2020-12-12 17:38:06 -0600

    Main Route added

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const { WeddingRoute }  = require('./routes/WeddingRoutes');
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/',(req, res, next) => {
+    res.json({
+        chekedIn: '/wedding/chekedIn',
+        cormfirm: '/weddingcormfirm',
+        wating  : '/wedding/wating'  ,
+        manually: '/wedding/manually',
+    })
+})
+
 WeddingRoute(app);
 
 app.listen(PORT,() => console.log(`http://localhost:${PORT}`));
